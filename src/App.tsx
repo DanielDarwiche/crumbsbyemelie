1382523d485931394e7b8f68b07ff63c09de8ab2
DanielDarwiche <danielinti1@hotmail.com>
2024-09-02 02:06:37 +0200

    route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import './AppMedia.css';
 import './Gallery.css';
@@ -19,6 +19,8 @@ function App(): JSX.Element {
         <Header />
         <main>
           <Routes>
+            {/* Omdirigera från root till /hem */}
+            <Route path="/" element={<Navigate replace to="/hem" />} />
             <Route path="/hem" element={<HomePage />} />
             <Route path="/galleri" element={<Gallery />} />
             <Route path="/galleri/:albumPath" element={<Pictures />} />
